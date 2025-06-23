9ac71b1a169191a14339140aa32cc904034e890d
vaisakh pb <vaisakhpb2005@gmail.com>
2025-06-23 13:56:10 +0530

    Initial project commit

---
+++ frontend/Home.js
@@ -0,0 +1,12 @@
+// Home.jsx
+function Home() {
+  return (
+    <div>
+      <h1>John Doe</h1>
+      <p>Email: johndoe@example.com</p>
+      <p>Phone: 123-456-7890</p>
+    </div>
+  );
+}
+
+export default Home;
